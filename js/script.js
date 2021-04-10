599fc9ca4cda413c160cd197a51ade63bf9cbd5e
Softonic254 <rotichenoch27@gmail.com>
2021-04-10 16:28:35 +0300

    It will add initial commit for my khan project

---
+++ js/script.js
@@ -0,0 +1,80 @@
+
+function calculate() {
+
+    var dayOfTheWeek = (year() + month() + century() + day()) % 7;
+
+    alert(Math.floor(dayOfTheWeek));
+
+}
+
+function gender() {
+    var gender = document.getElementById("gender");
+    var genderName = parseInt(gender.options[gender.selectedIndex].value);
+    return genderName;
+}
+
+function day() {
+    var day = document.getElementById("day").value;
+    // var value = parseInt(day.options[day.selectedIndex].value);
+
+    var date = parseInt(day);
+
+    if (date <= 0 || date > 31) {
+        alert("Invalid day input.")
+    } else {
+        return date;
+    }
+}
+
+function month() {
+    var month = document.getElementById("month");
+    var monthValue = parseInt(month.options[month.selectedIndex].value);
+    return monthValue;
+}
+
+function year() {
+    var year = document.getElementById("year");
+    var birthYear = year.options[year.selectedIndex].text;
+    var Y = birthYear.slice(2, 4);
+    var YY = parseInt(Y);
+    var yearCode = (YY + (YY / 4)) % 7
+    return yearCode;
+}
+
+function century() {
+    var century = document.getElementById("year");
+    var birthCentury = century.options[century.selectedIndex].text;
+    var C = birthCentury.slice(0, 2);
+    var CC = parseInt(C);
+    if (CC == 19) {
+        var centuryCode = 0;
+        return centuryCode;
+    }
+}
+
+function leapYearCode() {
+    var year = document.getElementById("year");
+    var birthYear = year.options[year.selectedIndex].text;
+    var Y = birthYear.slice(2, 4);
+    var leap = parseInt(Y);
+
+}
+
+function name() {
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua",]
+
+    Female
+    Sunday: Akosua
+
+    Monday: Adwoa
+
+    Tuesday: Abenaa
+
+    Wednesday: Akua
+
+    Thursday: Yaa
+
+    Friday: Afua
+}
